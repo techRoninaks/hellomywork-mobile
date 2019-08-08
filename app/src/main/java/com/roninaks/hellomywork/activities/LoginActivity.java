d2b5bb72e3daad0aebe713953a253d71f4ca390f
tech@roninaks.com <tech@roninaks.com>
2019-08-08 23:37:36 +0530

    Initail commit

---
+++ app/src/main/java/com/roninaks/hellomywork/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.roninaks.hellomywork.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roninaks.hellomywork.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //This is a test commit
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
